29b9788c0951779ea225ae7420f43bc55ce865a0
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2024-01-11 23:55:23 +0200

    add dependencies

--- app/src/main/java/com/example/storyscope/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/storyscope/ui/search/SearchFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.storyscope.R
-import com.example.storyscope.ui.book_details.ARG_PARAM1
-import com.example.storyscope.ui.book_details.ARG_PARAM2
 
 /**
  * A simple [Fragment] subclass.
@@ -15,17 +13,16 @@ import com.example.storyscope.ui.book_details.ARG_PARAM2
  * create an instance of this fragment.
  */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,13 +42,6 @@ class SearchFragment : Fragment() {
          * @return A new instance of fragment SearchFragment.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
