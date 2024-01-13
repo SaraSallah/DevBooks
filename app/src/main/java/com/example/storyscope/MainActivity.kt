7a4d0418d3a86edc76812ef0645d793b74148754
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2024-01-13 23:08:30 +0200

    add navigation graph

--- app/src/main/java/com/example/storyscope/MainActivity.kt
+++ app/src/main/java/com/example/storyscope/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.storyscope
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.storyscope.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_host) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
